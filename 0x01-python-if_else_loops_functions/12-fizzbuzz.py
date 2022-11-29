877aebc0155499036a616357cfdee479a1a9dfde
remmysilver <silverremmy@gmail.com>
2022-11-29 12:52:36 -0800

    correction

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-FIZZ = "Fizz"
-BUZZ = "Buzz"
 
 
 def fizzbuzz():
-    for number in range(1, 101):
-        if (number % 3 and number % 5):
-            print("%s%s" % (FIZZ, BUZZ), end=' ')
-        elif (number % 3):
-            print("%s" % (FIZZ), end=' ')
-        elif (number % 5):
-            print("%s" % (BUZZ), end=' ')
+    for n in range(1, 101):
+        if n % 5 == 0 and n % 3 == 0:
+            print('FizzBuzz', end=' ')
+        elif n % 3 == 0:
+            print('Fizz', end=' ')
+        elif n % 5 == 0:
+            if n == 100:
+                print('Buzz', end=' ')
+            else:
+                print('Buzz', end=' ')
         else:
-            print("%d" % (number), end=' ')
+            print('{:d}'.format(n), end=' ')
+
+
+if __name__ == '__main__':
+    fizzbuzz()
