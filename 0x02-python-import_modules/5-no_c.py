60b803fdaf6b7dee0a934b5f1d2547860e8e5de6
remmysilver <silverremmy@gmail.com>
2022-12-04 03:30:11 -0800

    solved;

--- 0x02-python-import_modules/5-no_c.py
+++ 0x02-python-import_modules/5-no_c.py
@@ -2,5 +2,15 @@
 
 
 def no_c(my_string):
-    copy_str = [x for x in my_string if x != 'c' or x != 'C']
-    return ("".join(copy_str))
+    new_list = list(my_string)
+    new_string = ''
+    for char in new_list:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
+
+
+if __name__ == '__main__':
+    print(no_c("Holberton School"))
+    print(no_c("Chicago"))
+    print(no_c("C is fun!"))
