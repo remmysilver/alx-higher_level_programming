53f99af739186220d904b25d70f771fbd4150193
remmysilver <silverremmy@gmail.com>
2023-01-10 16:03:34 -0800

    done

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""Defines a string-to-JSON function."""
+"""
+contain the JSON string
+"""
+
 import json
 
 
-
 def to_json_string(my_obj):
-    """Return the JSON representation of a string object."""
+    """returns the JSON representation of an object (string)"""
     return json.dumps(my_obj)
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
-
-
- a class Student."""
+"""
+Contains the clas "Student"
+"""
 
 
 class Student:
-    """Represent a student."""
-
+    """Representation of a student"""
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
+        """Initializes the student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Get a dictionary representation of the Student."""
+        """returns a dictionary representation of a Student instance"""
         return self.__dict__
