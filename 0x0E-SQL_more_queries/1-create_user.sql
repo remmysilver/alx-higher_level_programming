2eb1ad83c85d2932e9a7711b512357f1d80340f9
remmysilver <silverremmy@gmail.com>
2023-02-16 14:45:00 -0800

    done

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,7 @@
--- Create new user `user_0d_1`
-GRANT ALL PRIVILEGES ON *.*
-      TO 'user_0d_1'@'localhost'
-      IDENTIFIED BY 'user_0d_1_pwd';
+-- Creates the user user_0d_1 with all privileges.
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost';
