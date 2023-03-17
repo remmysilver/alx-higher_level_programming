2edcf4183c6a642f065b52cc63e99e6255148fc1
remmysilver <silverremmy@gmail.com>
2023-03-17 15:24:07 -0700

    objects and classes

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,28 +1,5 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-  rotate () {
-    let tmp = this.width;
-    this.width = this.height;
-    this.height = tmp;
-  }
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-}
-
-module.exports = class Square extends Rectangle {
+module.exports = class Square extends require('./4-rectangle.js') {
   constructor (size) {
     super(size, size);
   }
