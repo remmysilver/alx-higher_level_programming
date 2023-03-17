17fd0c06caa49fdc4d82d584c666d2dc0af5391f
remmysilver <silverremmy@gmail.com>
2023-03-17 15:37:02 -0700

    push 1

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,14 +1,17 @@
 #!/usr/bin/node
+const dict = require('./101-data').dict;
 
-let dict = require('./101-data').dict;
-let d = {};
-
-for (let k in dict) {
-  if (!(dict[k] in d)) {
-    d[dict[k]] = [k];
-  } else {
-    d[dict[k]].push(k);
+const totalist = Object.entries(dict);
+const vals = Object.values(dict);
+const valsUniq = [...new Set(vals)];
+const newDict = {};
+for (const j in valsUniq) {
+  const list = [];
+  for (const k in totalist) {
+    if (totalist[k][1] === valsUniq[j]) {
+      list.unshift(totalist[k][0]);
+    }
   }
+  newDict[valsUniq[j]] = list;
 }
-
-console.log(d);
+console.log(newDict);
