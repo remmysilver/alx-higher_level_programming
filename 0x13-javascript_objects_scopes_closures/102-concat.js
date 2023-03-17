e2a2400f884a48906b027694e7121ed24ec62ba3
remmysilver <silverremmy@gmail.com>
2023-03-17 15:19:20 -0700

    objects and classes

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-let args = process.argv.slice(2);
-if (args.length < 3) {
-  console.log('Usage: ./102-concat.js fileA fileB fileC');
-  process.exit(1);
-}
-let fa = fs.readFileSync(args[0]);
-let fb = fs.readFileSync(args[1]);
-fs.writeFileSync(args[2], fa + fb);
+const a = fs.readFileSync(process.argv[2], 'utf8');
+const b = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], a + b);
