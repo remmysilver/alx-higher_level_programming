98c9c18acdc6b5ffe9e15ac4c5ec22bc496f9fa2
remmysilver <silverremmy@gmail.com>
2023-03-31 01:39:14 -0700

    correction on task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,37 +1,26 @@
 #!/usr/bin/python3
+"""Defines a peak-finding algorithm."""
 
 
 def find_peak(list_of_integers):
-    """Find a peak in a list of integers.
+    """ Finds the peak in a list of integers """
+    if list_of_integers == []:
+        return None
 
-    Example:
-        [1, 2, 4, 6, 3] -> 6
-        [4, 2, 1, 2, 3, 1] -> 3 # but 4 is also a peak
+    length = len(list_of_integers)
+    m = int(length / 2)
+    li = list_of_integers
 
-    Returns: integer that is the peak.
-    """
-    l = len(list_of_integers)
-    n = l // 2
-    v = 0
-    while v < l:
-        v += 1
-        if n == 0:
-            gtl = True
-        else:
-            gtl = False
-        if n == l - 1:
-            gtr = True
-        else:
-            gtr = False
-        if n > 0:
-            if list_of_integers[n - 1] < list_of_integers[n]:
-                gtl = True
-        if n < l - 1:
-            if list_of_integers[n] > list_of_integers[n + 1]:
-                gtr = True
-        if gtl and gtr:
-            return list_of_integers[n]
-        if not gtr:
-            n += (l - n) // 2
-        elif not gtl:
-            n -= n // 2
+    if m - 1 < 0 and m + 1 >= length:
+        return li[m]
+    elif m - 1 < 0:
+        return li[m] if li[m] > li[m + 1] else li[m + 1]
+    elif m + 1 >= length:
+        return li[m] if li[m] > li[m - 1] else li[m - 1]
+
+    if li[m - 1] < li[m] > li[m + 1]:
+        return li[m]
+
+    if li[m + 1] > li[m - 1]:
+        return find_peak(li[m:])
+    return find_peak(li[:m])
