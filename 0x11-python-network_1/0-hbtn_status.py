ec0ffb17dffd1705fd4c9c1f37d563cc804fa37e
remmysilver <silverremmy@gmail.com>
2023-03-31 04:03:49 -0700

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""Script to request content from `https://intranet.hbtn.io/status`"""
-
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
-import urllib.error
 
-try:
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as r:
-        body = r.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(body)))
-        print('\t- content: {}'.format(body))
-        print('\t- utf8 content: {}'.format(body.decode('utf8')))
-except urllib.error.URLError as e:
-    print(e)
+
+if __name__ == "__main__":
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
